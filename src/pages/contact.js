da19ae9a78cf040e24a8bc29d603727c98eaa19e
Michael Sheehan <sheehanpmichael@gmail.com>
2019-01-29 00:35:17 +0000

    ver 1.9

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,13 +18,16 @@ const ContactPage = () => (
             <form
                 name='contact-form'
                 method="post" data-netlify="true"
-                // data-netlify-honeypot="bot-field"
-                action="/confirmation"
+            // data-netlify-honeypot="bot-field"
+            // action="/confirmation"
             >
                 {/* <input type="hidden" name="bot-field" /> */}
-                <label htmlFor='name' ><input name='name' placeholder="Your Name.." type='text' /> </label>
-                <label htmlFor='email' > <input name='email' placeholder="Your email" type='email' /></label>
-                <label htmlFor='message' > <textarea className="text-area" name='message' placeholder="Your Message" type='text-area' required /> </label>
+                <label htmlFor='name' ><input name='name' placeholder="Your Name" type='text' id='name' /> </label>
+                <label htmlFor='email' > <input name='email' placeholder="Your email" type='email' id='email' /></label>
+                <label htmlFor='message' > <textarea className="text-area" name='message' placeholder="Your Message"
+                    // type='text-area' 
+                    id='message'
+                    required /> </label>
 
                 <button className="btn btn--full" type="submit">Send</button>
             </form>
