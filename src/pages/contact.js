e64e257da7f21bd49b908b713b80ea66c9d209ee
Michael Sheehan <sheehanpmichael@gmail.com>
2019-01-29 11:30:59 +0000

    ver 2.0.1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,24 +16,46 @@ const ContactPage = () => (
         }}>
 
             <form
-                name='contact-form-1'
+                name="contact-form-1"
                 method="post"
                 data-netlify="true"
-                data-netlify-honeypot="bot-field"
+                // data-netlify-honeypot="bot-field"
+                netlify-honeypot="bot-field"
             // action="/confirmation"
             >
-                {/* <input type="hidden" name="bot-field" /> */}
-                <input type="hidden" name="contact-form-1" value="contact" />" and "<p hidden> <label htmlFor="bot-field">Don’t fill this out:{' '}<input name="bot-field" /> </label> </p>
+                <p class="hidden">
+                    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+                </p>
 
-                <label htmlFor='name' ><input name='name' placeholder="Your Name" type='text' id='name' /> </label>
-                <label htmlFor='email' > <input name='email' placeholder="Your email" type='email' id='email' /></label>
-                <label htmlFor='message' > <textarea className="text-area" name='message' placeholder="Your Message"
-                    // type='text-area' 
-                    id='message'
-                    required /> </label>
+                <label htmlFor='name' >
+                    <input
+                        name='name'
+                        placeholder="Your Name"
+                        type='text'
+                        id='name'
+                    />
+                </label>
+                <label htmlFor='email' >
+                    <input
+                        name='email'
+                        placeholder="Your email"
+                        type='email'
+                        id='email'
+                    />
+                </label>
+                <label htmlFor='message' >
+                    <textarea
+                        className="text-area"
+                        name='message'
+                        placeholder="Your Message"
+                        id='message'
+                        required
+                    />
+                </label>
 
                 <button className="btn btn--full" type="submit">Send</button>
             </form>
+
         </div>
     </Layout>
 )
